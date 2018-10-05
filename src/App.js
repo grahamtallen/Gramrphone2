da7de276d1c63e6e165217b0648ee1ace01719e6
Graham Allen <grahamtallen94@gmail.com>
2018-10-06 01:16:26 +0300

    responsive nav bar with responsive routes

--- src/App.js
+++ src/App.js
@@ -3,36 +3,22 @@ import {
   BrowserRouter as Router,
   Route,
   Link
-} from 'react-router-dom'
-import './App.css'
+} from 'react-router-dom';
+import NavBar, {routes} from './components/NavBar/NavBar';
+import './App.css';
 
-import Session from './views/Session.js'
-
-const About = () => (
-  <div>
-    <h2>About</h2>
-  </div>
-)
-
-const Topic = ({ match }) => (
-  <div>
-    <h3>{match.url}</h3>
-  </div>
-)
 
 
 const App = () => (
   <Router>
     <div>
-      <div className="top-bar">
-        <div className="link-btn"><Link to="/">Session</Link></div>
-        <div className="link-btn"><Link to="/about">Artists</Link></div>
-        <div className="link-btn"><Link to="/topics">Chat</Link></div>
-      </div>
-
-      <Route exact path="/" component={Session}/>
-      <Route path="/about" component={About}/>
-      <Route path="/topics" component={Topic}/>
+      <NavBar />
+      {routes.map(route => {
+        console.log(route.path)
+        return (
+            <Route {...route}  path={route.home ? "/" : "/" + route.path} />
+        )
+      })}
     </div>
   </Router>
 )
---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,86 @@
+import "./NavBar.css";
+import React from 'react';
+import {capitalize} from '../../utils/strings';
+import Session from '../../views/Session.js'
+import { Link } from 'react-router-dom';
+
+const About = () => (
+  <div>
+    <h2>About</h2>
+  </div>
+)
+
+const Topic = ({ match }) => (
+  <div>
+    <h3>{match.url}</h3>
+  </div>
+)
+
+
+export default class NavBar extends React.Component {
+
+	state = {
+		sidebarOpen: false
+	}
+
+	toggleSidebar = () => {
+		this.setState({sidebarOpen: !this.state.sidebarOpen})
+	}
+
+
+	render() {
+		const {state} = this;
+		const {sidebarOpen} = state;
+		return (
+			<div className="top-bar">
+				<div className="wide-screen-nav">
+			        {
+			        	routes.map((route) => {
+			        		return (
+			        			<div className={`link-btn`}>
+			        				<Link to={route.home ? "/" : route.path}>
+			        					{capitalize(route.path)}
+			        				</Link>
+		        				</div>
+			        		)
+			        	})
+			        }
+		        </div>
+		        <div className="small-screen-nav">
+			        <button className="w3-button" onClick={this.toggleSidebar}>&#9776;</button>
+					<div className="w3-sidebar w3-bar-block w3-collapse w3-card w3-animate-left" style={{width: "200px", display: sidebarOpen ? "block" : "none"}}>
+					  <button className="w3-bar-item w3-button w3-large w3-hide-large" onClick={this.toggleSidebar} >Close &times;</button>
+					 {
+			        	routes.map((route) => {
+			        		return (
+			        			<div className={`w3-bar-item w3-button link-btn`}>
+			        				<Link to={route.home ? "/" : route.path}>
+			        					{capitalize(route.path)}
+			        				</Link>
+		        				</div>
+			        		)
+			        	})
+			        }
+					</div>
+				</div>
+		    </div>
+		)
+	}
+}
+
+export const routes = [
+	{
+		path: "session",
+		home: true,
+		exact: true,
+		component: Session
+	},
+	{
+		path: "artists",
+		component: About
+	},
+	{
+		path: "chat",
+		component: Topic
+	}
+]
